dfbfbb43cb8464bb9c629b4f32adeff5b919d0fe
Andreapf13 <pastorfernandezandrea@gmail.com>
2023-03-27 17:47:00 +0200

    third commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
-    import Preview from "./Preview.svelte";
+import Preview from "./Preview.svelte" 
 </script>
-<Preview titulo="Los trabajos de extinción frenan el incendio de Castellón y Teruel a las puertas de un parque natural" texto="El operativo afronta este lunes el día más complicado por las fuertes rachas de viento"/>
+
+<Preview/>
--- src/Preview.svelte
+++ src/Preview.svelte
@@ -1,17 +1,13 @@
 <script>
-  export let  titulo;
-  export let texto;
+ const noticia = {
+    titulo: 'Los trabajos de extinción frenan el incendio de Castellón y Teruel a las puertas de un parque natural',
+    preview: 'El operativo afronta este lunes el día más complicado por las fuertes rachas de viento.',
+    contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+  };
+
 </script>
 
-<h1> {titulo}</h1>
-<p>{texto}</p>
+<h2>EL PAÍS : {noticia.titulo}</h2>
+<p>En el día de hoy {noticia.preview}</p>
+<p>Además {noticia.contenido}</p>
 
-<style>
-h1{
-color:blue;
-text-align:center;
-}
-p{
-    text-align:center;
-}
-</style>
