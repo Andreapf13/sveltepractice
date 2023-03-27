9b607d1c90e5d0264f1435b6997b0b5cea7f4fc9
Andreapf13 <pastorfernandezandrea@gmail.com>
2023-03-27 17:20:35 +0200

    firstcard

---
+++ src/Preview.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let  titulo;
+  export let texto;
+</script>
+
+<h1> {titulo}</h1>
+<p>{texto}</p>
+
+<style>
+h1{
+color:blue;
+text-align:center;
+}
+p{
+    text-align:center;
+}
+</style>
