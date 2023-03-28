adee29c2db055e0cdaa9bfd756e15e2fb23ea7ad
Andreapf13 <pastorfernandezandrea@gmail.com>
2023-03-28 12:21:21 +0200

    Practice Each

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,42 @@
 <script>
     import PreviewComponent from "./PreviewComponent.svelte";
     //Definimos el objeto
- let noticia = {
-    titulo: 'Los trabajos de extinción frenan el incendio de Castellón y Teruel a las puertas de un parque natural',
-    preview: 'El operativo afronta este lunes el día más complicado por las fuertes rachas de viento.',
-    contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' }
+    let noticias = [
+    {
+        titulo: 'Un nuevo estudio revela una forma efectiva de prevenir el estrés laboral',
+        preview: 'La investigación sugiere que un aumento de actividades de ocio puede mejorar el bienestar en el trabajo.',
+        contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        titulo: 'Descubren una nueva especie de ave en la selva amazónica',
+        preview: 'La nueva especie de ave fue encontrada en una zona remota de la selva amazónica y su descubrimiento ha generado gran interés entre los expertos.',
+        contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        titulo: 'El nuevo sistema de transporte público de la ciudad recibe críticas',
+        preview: 'Los usuarios se quejan de largos tiempos de espera y retrasos en los horarios, mientras que los operadores aseguran estar trabajando para mejorar el servicio.',
+        contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        titulo: 'La nueva película de ciencia ficción promete ser todo un éxito',
+        preview: 'La crítica especializada alaba la calidad del guión y los efectos especiales, y el público espera ansioso su estreno.',
+        contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        titulo: 'El equipo de fútbol local logra un importante triunfo en casa',
+        preview: 'El equipo local consiguió una victoria por goleada en su propio estadio, y los aficionados celebraron la hazaña en las calles de la ciudad.',
+        contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim'}
+    ]
 </script>
 
 
-<PreviewComponent noticia={noticia} />
+<div class="portada">
+    {#each noticias as noticia}
+      <div class="preview">
+        <h2>{noticia.titulo}</h2>
+        <p>{noticia.preview}</p>
+      </div> 
+    {/each}
+  </div>
 
 
