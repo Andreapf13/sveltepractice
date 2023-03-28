448a3121fd8f2fe2e9dffe2ca294f1921cfd0431
Andreapf13 <pastorfernandezandrea@gmail.com>
2023-03-28 11:45:49 +0200

    Correct Error

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script>
-import Preview from "./Preview.svelte" 
+    import PreviewComponent from "./PreviewComponent.svelte";
+    //Definimos el objeto
+ let noticia = {
+    titulo: 'Los trabajos de extinción frenan el incendio de Castellón y Teruel a las puertas de un parque natural',
+    preview: 'El operativo afronta este lunes el día más complicado por las fuertes rachas de viento.',
+    contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' }
 </script>
 
-<Preview/>
+
+<PreviewComponent noticia={noticia} />
+
+
