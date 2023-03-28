40d2bfc4a7625027caee30859739ae26c7cdd038
Andreapf13 <pastorfernandezandrea@gmail.com>
2023-03-28 12:02:08 +0200

    Correct Error

--- src/PreviewComponent.svelte
+++ src/PreviewComponent.svelte
@@ -1,13 +1,10 @@
 <script>
- const noticia = {
-    titulo: 'Los trabajos de extinción frenan el incendio de Castellón y Teruel a las puertas de un parque natural',
-    preview: 'El operativo afronta este lunes el día más complicado por las fuertes rachas de viento.',
-    contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
-  };
-
+export let noticia;
 </script>
 
-<h2>EL PAÍS : {noticia.titulo}</h2>
-<p>En el día de hoy {noticia.preview}</p>
-<p>Además {noticia.contenido}</p>
 
+<div class="noticia">
+    <h2>{noticia.titulo}</h2>
+    <p>{noticia.preview}</p>
+    <p>{noticia.contenido}</p>
+</div>
